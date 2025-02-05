60189fd14b3200b45b932b4b175eb2f7dec42497
emno0001 <emno0001@stud.kea.dk>
2025-02-05 11:06:07 +0100

    done sale og sold out

--- produktliste.js
+++ produktliste.js
@@ -1,29 +1,33 @@
 const mycategory = new URLSearchParams(window.location.search).get("category");
-console.log("produktliste loader... med category:",mycategory);
+document.querySelector("h1").innerHTML = mycategory;
+console.log("produktliste loader... med category:", mycategory);
 const product_list_containe = document.querySelector(".product_list_containe");
 
 fetch(`https://kea-alt-del.dk/t7/api/products?category=${mycategory}`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
-function showList(products){
-    const markup = products
-      .map(
-        (product) =>
-          `
-        
+function showList(products) {
+  console.log("siden", products);
+  const markup = products
+    .map(
+      (product) =>
+        `
         <div class="box">
-                <img class="produkt_img" src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="">
-                <h3>${product.productdisplayname}</h3>
-                <p class="line">${product.articletype} - ${product.brandname}</p>
-                <p>${product.category}
-                <div class="pris_container">
-                    <p>DKK ${product.price},-</p>
-                    <p class="rabat hide">0%</p>
-                </div>
-                <a href="produkt.html">Read more</a>
-        </div>`
-      )
-      .join("");
-    product_list_containe.innerHTML = markup;
+    ${product.soldout ? '<div class="text-box">Sold Out</div>' : ""}
+    <img class="produkt_img ${product.soldout ? "sold_out" : ""}" src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="">
+    <h3>${product.productdisplayname}</h3>
+    <p class="line">${product.articletype} - ${product.brandname}</p>
+    <p>${product.category}</p>
+    <div class="pris_container">
+        <p>DKK ${product.price},-</p>
+        <p class="rabat ${product.discount ? "isonsale" : ""}">-${product.discount}%</p>
+    </div>
+    <a href="produkt.html?product=${product.id}">Read more</a>
+</div>
+        
+        `
+    )
+    .join("");
+  product_list_containe.innerHTML = markup;
 }
